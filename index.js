9bd8dcdbe0faa3615f36e3fc56c154fc1f118d61
baronlip <baronlip@gmail.com>
2019-04-27 22:10:14 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,5 +0,0 @@
-
-
-// 4) Log sayHiToGrandma(string) returns "I can't hear you!" if "string" is lowercase.
-// 5) sayHiToGrandma(string) returns "YES INDEED" if "string is uppercase"
-// 6) sayHiToGrandma(string) returns "I love you, too." if "string" is "I love you, Grandma."
