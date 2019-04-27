e0db6d68f3fc939661f25a91d2910970877db8da
baronlip <baronlip@gmail.com>
2019-04-27 21:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function shout(string) {
   return string.toUpperCase()
 }
 
+// 1) 
+function whisper(string) {
+  return string.toLowerCase
+}
+
+// 2) Log shout
+console.log(shout("I'm shouting"))
+
