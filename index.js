8ed0921ecc137418808b6c6e469efce06eb80fb8
baronlip <baronlip@gmail.com>
2019-04-27 22:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-const whisper = function whisper(string) {
-  return console.log(string.toLowerCase())
-}
 
-
-
-const shout = function shout(string) {
-  return console.log(string.toUpperCase())
-}
-
-
-console.log(whisper("HELLO"));
-
-
-console.log(shout("hello"))
-
-
-const sayHiToGrandma = function logSayHiToGrandma(string) {
-  if (string.toLowerCase === lowercase ) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase === uppercase) {
-    return "YES INDEED"
-  } else {
-    return "I love you, too."
-  }
-}
 
 // 4) Log sayHiToGrandma(string) returns "I can't hear you!" if "string" is lowercase.
 // 5) sayHiToGrandma(string) returns "YES INDEED" if "string is uppercase"
